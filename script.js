9d2716cf38982213dea83d1957e665ddcead5488
Shaden Duaibes <sally.daibes@outlook.com>
2024-08-20 14:24:47 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function f1()
+{
+   alert(' you must sign in to see the content of this feature');
+}
+function f2()
+{
+   alert(' you must sign in to see the content of this arrivels');
+}
+function learnmore()
+{
+    alert('Sign in to see more detials about as ')
+}
+function showEmail() {
+    const email = "example@example.com";
+    alert("البريد الإلكتروني هو: " + email);
+}
